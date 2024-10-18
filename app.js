90e7002e27fab58d5507166ff841f04f18bcc499
ravi-64bit <107312602+ravi-64bit@users.noreply.github.com>
2024-10-18 21:32:30 +0530

    use 386 to redesign front-end

--- app.js
+++ app.js
@@ -2,25 +2,32 @@
 const express=require('express');
 const ejs=require('ejs');
 const fetch=require('node-fetch');
+const bodyp=require('body-parser');
+const name=require('sillyname');
+//const superhero=require('superheroes');
 
 //app
 const app=express();
 app.use(express.static('public'));
 app.set('view engine','ejs');
 
+//list of APIs used
+APIList=['joke','numbers'];
+
+
 //root
 app.get("/",(req,res)=>{
-    res.render('index',{name:'Raj Bihari'});
+    res.render('index',{name:name()});
 });
 
 
 //joke API 1
 app.get('/joke',async (req,res)=>{
     try{
-        const response=await fetch("https://v2.jokeapi.dev/joke/Any?type=single");
+        const response=await fetch("https://v2.jokeapi.dev/joke/Miscellaneous,Pun,Spooky,Christmas?type=single");
         const apidata=await response.json();
-        const joke=apidata.joke;
-        res.render('joke',{type:" ",joke:joke});
+        //const joke=apidata.joke;
+        res.render('joke',{type:" ",joke:apidata.joke});
     }
     catch(exc){
         res.status(500).send('error 500');
@@ -39,8 +46,24 @@ app.get("/progjoke",async (req,res)=>{
     res.render('joke',{type:"programming",joke:apidata.joke});
 });
 
+app.get("/dadjoke",async (req,res)=>{
+    const response=await fetch("https://icanhazdadjoke.com/",{headers:{'Accept':'application/json'}});
+    const dadjokejson=await response.json();
+    res.render('joke',{type:'dad',joke:dadjokejson.joke});
+});
+
+//numbers api
+app.get('/numbers',async (req,res)=>{
+    var input=900;
+    var url="https://numbersapi.com/random";
+    const response=await fetch(url,{headers:{'Accept':'application/json'}});
+    const numberfact=await response.json();
+    console.log(numberfact);
+});
+
+
 //assigning a port
-app.listen('30',(error)=>{
+app.listen('3000',(error)=>{
     if(error){
         console.log(error);
     }
---
+++ test.js
@@ -0,0 +1,3 @@
+const name=require('superheroes');
+
+console.log(name.randomSuperhero)
