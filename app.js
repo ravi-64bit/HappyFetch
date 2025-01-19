f1c3e2a5f9fd1839ec8c6cd10d83e062f89c8360
ravi-64bit <107312602+ravi-64bit@users.noreply.github.com>
2025-01-19 21:41:22 +0530

    add error page

--- app.js
+++ app.js
@@ -61,6 +61,10 @@ app.get('/numbers',async (req,res)=>{
     res.send(numberfact);
 });
 
+app.use((req,res,next)=>{
+    res.status(404).render('404');
+}
+
 
 //assigning a port
 app.listen('3000',(error)=>{
